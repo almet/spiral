67775d17c9328b58c090a4654dab3a04e22a6147
ametaireau@ecureuil <none@none>
2009-04-09 19:22:23 +0200

    Removing some old classes

--- tests/index.php
+++ tests/index.php
@@ -4,10 +4,15 @@ use Spiral\tests\Youpa as Youpa;
 require_once('../Core/ini.php');
 $schema = new Schema();
 
-$schema->registerService('testClass', 'Spiral\\tests\\ToInject')
+$schema
+->registerService('testClass', 'Spiral\\tests\\ToInject')
     ->onConstruct()->injectWith("content injected in youpi constructor \n", 'appel dynamique')
     ->onCall('myMethod')->injectWith('injection', 'de', 'methode dynamique')
     ->onStaticCall('Spiral\\tests\\Yataa', 'myStaticMethod')->injectWith(Schema::SELF)
+    ->onCall('serviceNeeded')->injectWithServices('yataa')
+
+->registerService('yataa', 'Spiral\\tests\\Yataa')
+
 ;
 $container = new Spiral\Core\Di\Container($schema);
 $injectedObject = $container->testClass;
