d13dbeee4c52e10f6f43d806639068790507ffbd
Alexis Metaireau <ametaireau@gmail.com>
2009-07-28 22:41:17 +0200

    Bootstrap fixes to work correctly with namespaces

---
+++ Framework/Bootstrap/Classes/Exception.php
@@ -0,0 +1,13 @@
+<?php
+namespace Spiral\Framework\Bootstrap;
+/**
+ * Default Bootstrap Exception
+ *
+ * Base exception for all Bootstraps exceptions. This is the only Exception
+ * visible outside the package.
+ *
+ * @author		Alexis Métaireau	28 jul. 2009
+ * @copyright	Alexis Metaireau	2009
+ * @licence		GNU/GPL V3. Please see the COPYING FILE. 
+ */
+class Exception extends \Exception{};
---
+++ Framework/Bootstrap/Classes/Exception/FileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Spiral\Framework\Bootstrap\Exception;
+
+use \Spiral\Framework\Bootstrap\Exception;
+
+/**
+ * Exception thrown when a file is not found
+ * 
+ * @author		Alexis Métaireau	28 jul. 2009
+ * @copyright	Alexis Metaireau	2009
+ * @licence		GNU/GPL V3. Please see the COPYING FILE. 
+ */
+class FileNotFoundException extends Exception{};
