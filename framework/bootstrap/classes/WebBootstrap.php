74a9108bffb782191222f538a104329c09cde2f8
fred <none@none>
2009-10-27 22:45:37 -0400

    Web bootstrap added
    This web bootstrap should be improved later.

---
+++ framework/bootstrap/classes/WebBootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace spiral\framework\bootstrap;
+
+require_once 'Bootstrap.php';
+require_once 'PackageLoader.php';
+
+/**
+ * Web bootstrap
+ * 
+ * Launch an application from web interface.
+ *
+ * @author		Frédéric Sureau <fred@spiral-project.org>
+ * @copyright	2009 Spiral-project.org <http://www.spiral-project.org>
+ * @license		GNU General Public License <http://www.gnu.org/licenses/gpl.html>
+ */
+class WebBootstrap implements Bootstrap
+{
+	/**
+	 * Bootstrap the application
+	 *
+	 * @return 	void
+	 */
+	public function run()
+	{
+		// Define include path
+		$spiralRootPath = __DIR__.DIRECTORY_SEPARATOR.'..';
+		set_include_path( $spiralRootPath .PATH_SEPARATOR. get_include_path() );
+		
+		// Configure the package loader and register it in the SPL autoload queue
+		PackageLoader::addSearchDirectory('Classes');
+		spl_autoload_register('\Spiral\Framework\Bootstrap\PackageLoader::load');
+		
+		// Run the application
+		// FIXME Use a MVC front request handler
+		// Waiting for the MVC, you can use the public/test.php file to enjoy
+		require 'public/test.php';
+	}
+}
